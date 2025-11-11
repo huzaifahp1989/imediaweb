bbce1014cc214572f09e9b869a7d7014872d8961
huzaifahp1989 <huzaify786@gmail.com>
2025-11-11 14:22:56 +0000

    Add TanStack React Query provider: wrap app in QueryClientProvider to fix 'No QueryClient set' error

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "@/App.jsx";
+// TanStack React Query setup
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // Ensure global styles and Tailwind CSS are loaded
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
