3ca467ba3caa151c0ff5bbd48604ee64c38db5ff
huzaifahp1989 <huzaify786@gmail.com>
2025-11-12 10:10:31 +0000

    feat(QuranDictionary): subtle highlight + legend, quick-jump and keyboard nav, export/import of user difficult marks; build /docs

--- docs/assets/zap.DzSkkUv7.js
+++ docs/assets/zap.DzSkkUv7.js
@@ -1,4 +1,4 @@
-import{q as a}from"./index.D4NvwcEx.js";/**
+import{q as a}from"./index.BTyQagyC.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
