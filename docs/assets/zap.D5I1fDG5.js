c66526cd293db0a1ca80eec1905a64c3d0ca0c66
huzaifahp1989 <huzaify786@gmail.com>
2025-11-12 09:51:49 +0000

    Publish: update /docs with new external Landing page as home and Kids Home route

--- docs/assets/zap.D5I1fDG5.js
+++ docs/assets/zap.D5I1fDG5.js
@@ -1,4 +1,4 @@
-import{q as a}from"./index.Cl3CnLBx.js";/**
+import{q as a}from"./index.BaQrC8Rc.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
